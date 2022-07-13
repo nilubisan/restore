6ca8691e652abe6b544863b9335e7678f46aa87d
nilubisan <r.nas9329@gmail.com>
2022-07-14 01:06:55 +0600

    feat: add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,13 @@ import './app.css';
 import { withBookstoreService } from '../hoc'
 import { Routes, Route } from 'react-router-dom'
 import { HomePage, CartPage } from '../pages'
+import ShopHeader from '../shop-header';
+
 
 const App = () => {
     return (
         <main role="main" className="container">
+            <ShopHeader numItems={5} total={210} />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/cart" element={<CartPage />} />
